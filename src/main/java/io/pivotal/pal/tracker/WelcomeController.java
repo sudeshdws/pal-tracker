a1b20187a88a46a5727719208add2e988c7ccae8
sudeshdws <sudeshdws@gmail.com>
2020-01-28 15:26:04 -0800

    Added manifest file

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,21 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+
+    String text ;
+
+    public WelcomeController(@Value("${WELCOME_MESSAGE}") String text) {
+        this.text =text;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return text;
     }
 }
