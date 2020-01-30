02ef28b252c3db6c6beaeee87603d0e95579b5fc
sudeshdws <sudeshdws@gmail.com>
2020-01-30 12:06:49 -0800

    Case added for actuater

--- src/test/java/test/pivotal/pal/trackerapi/HealthApiTest.java
+++ src/test/java/test/pivotal/pal/trackerapi/HealthApiTest.java
@@ -2,9 +2,8 @@ package test.pivotal.pal.trackerapi;
 
 import com.jayway.jsonpath.DocumentContext;
 import io.pivotal.pal.tracker.PalTrackerApplication;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -17,7 +16,7 @@ import static com.jayway.jsonpath.JsonPath.parse;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest(classes = PalTrackerApplication.class, webEnvironment = RANDOM_PORT)
 public class HealthApiTest {
 
@@ -25,11 +24,11 @@ public class HealthApiTest {
     private String port;
     private TestRestTemplate restTemplate;
 
-    @Before
+    @BeforeEach
     public void setUp() throws Exception {
         RestTemplateBuilder builder = new RestTemplateBuilder()
             .rootUri("http://localhost:" + port)
-            .basicAuthorization("user", "password");
+            .basicAuthentication("user", "password");
 
         restTemplate = new TestRestTemplate(builder);
     }
@@ -37,14 +36,16 @@ public class HealthApiTest {
     @Test
     public void healthTest() {
         ResponseEntity<String> response = this.restTemplate.getForEntity("/actuator/health", String.class);
-
+        System.out.println("-------------------------");
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-
+        System.out.println(response.getBody());
         DocumentContext healthJson = parse(response.getBody());
 
+        System.out.println("-------------------------");
+        System.out.println(healthJson);
         assertThat(healthJson.read("$.status", String.class)).isEqualTo("UP");
-        assertThat(healthJson.read("$.details.db.status", String.class)).isEqualTo("UP");
-        assertThat(healthJson.read("$.details.diskSpace.status", String.class)).isEqualTo("UP");
+        assertThat(healthJson.read("$.components.db.status", String.class)).isEqualTo("UP");
+        assertThat(healthJson.read("$.components.diskSpace.status", String.class)).isEqualTo("UP");
     }
 }
